61b617b759f9b86f9f4d0ff98499e51aca52ef15
HKUST-kwchanbs <145102156+HKUST-kwchanbs@users.noreply.github.com>
2023-11-10 18:13:48 +0800

    Update DataOfSquare.java

--- src/main/java/Game/DataOfSquare.java
+++ src/main/java/Game/DataOfSquare.java
@@ -6,15 +6,15 @@ public class DataOfSquare {
 	
 	//ArrayList that'll contain the colors
 	ArrayList<Color> C =new ArrayList<Color>();
-	int color; //3: Jerry, 2: Tom , 1: Wall, 0:empty
+	int color; //3: Jerry, 2: Tom, 1: Wall, 0:empty
 	SquarePanel square;
 	public DataOfSquare(int col){
 		
-		//Lets add the color to the arrayList
-		C.add(Color.white);	  //0
-		C.add(Color.darkGray);//1
-		C.add(Color.blue);	  //2
-		C.add(Color.orange);  //3
+		// Let's add the color to the arrayList
+		C.add(Color.white);	//0
+		C.add(Color.darkGray);	//1
+		C.add(Color.blue);	//2
+		C.add(Color.orange);  	//3
 		color=col;
 		square = new SquarePanel(C.get(color));
 	}
