07a17acc0efbc5463a6b8d210234a55fbcd6201d
Kamus <kwchanbs@connect.ust.hk>
2023-11-12 17:02:52 +0800

    modified GUI to set properties inside constructor

--- src/main/java/Game/Main.java
+++ src/main/java/Game/Main.java
@@ -8,13 +8,41 @@ public class Main {
 		//Creating the window with all its awesome snaky features
 		MazeGenerator maze = new MazeGenerator(30);
 		maze.generateMaze();
+
+		// Initial position of Jerry
+		Tuple StartLocation = new Tuple(0,0);
+
+		// Initial position of Tom / Target ending location of Jerry
+		Tuple EndLocation = new Tuple(0,0);
+
+		// Find the actual starting locations from the map
+		for(int j = 0; j < 30; j++){
+			if(maze.getMaze()[j][0] == 0){
+				StartLocation = new Tuple(0, j);
+				maze.getMaze()[j][0] = 3;
+			}
+
+			else if(maze.getMaze()[j][29] == 0){
+				EndLocation = new Tuple(29, j);
+				maze.getMaze()[j][29] = 2;
+			}
+		}
+
+		// Initialize the location of Jerry
+		JerryLocation jerry = new JerryLocation(StartLocation, EndLocation, 500, 1);
+		jerry.start();
+
+		// Initialize the location of Tom
+		TomLocation tom = new TomLocation(EndLocation, EndLocation, 400, 0);
+		tom.start();
+
+		// GUI
 		GUI f1 = new GUI(maze.getMaze());
-		
+
 		//Setting up the window settings
 		f1.setTitle("Tom and Jerry");
 		f1.setSize(300,300);
 		f1.setVisible(true);
-		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
-
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
